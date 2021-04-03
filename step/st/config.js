491994c591b0aca2730673bb2d80bd111d767fb2
studio-dg <81454965+studio-dg@users.noreply.github.com>
2021-04-04 07:37:34 +0900

    Update config.js

--- step/st/config.js
+++ step/st/config.js
@@ -2,7 +2,7 @@
 var config = {
   apiKey: "AIzaSyCAElLSoR2gD58y8A2QZdj12I-NCBnQa_Y",
   authDomain: "step-at.firebaseapp.com",
-  databaseURL: "https://test-f76bc.firebaseio.com",
+  databaseURL: "https://step-at.firebaseio.com",
   projectId: "step-at",
   storageBucket: "step-at.appspot.com",
   messagingSenderId: "G-FLKR07TPZY"
