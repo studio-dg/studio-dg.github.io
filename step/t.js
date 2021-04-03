f43be60f220af58cff5446999dd8be7e1631d10f
studio-dg <81454965+studio-dg@users.noreply.github.com>
2021-04-03 18:34:42 +0900

    Create t.js

---
+++ step/t.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAElLSoR2gD58y8A2QZdj12I-NCBnQa_Y",
+  authDomain: "step-at.firebaseapp.com",
+  projectId: "step-at",
+  storageBucket: "step-at.appspot.com",
+  messagingSenderId: "658028135386",
+  appId: "1:658028135386:web:30649e00320565a2e476e3",
+  measurementId: "G-FLKR07TPZY"
+};
