7c0b70428c626401ebcd66d50662c01787284785
studio-dg <81454965+studio-dg@users.noreply.github.com>
2021-04-21 08:52:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,36 @@ app_css.rel = 'stylesheet';
 app_css.type = 'text/css';
 document.getElementsByTagName('head')[0].appendChild(app_css);
 };
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+var queryString = window.location.search;
+var queryObject = new Object();
+if(queryString){
+  queryString = queryString.substring(1);
+  var parameters = queryString.split('&');
+
+  for (var i = 0; i < parameters.length; i++) {
+    var element = parameters[i].split('=');
+
+    var paramName = decodeURIComponent(element[0]);
+    var paramValue = decodeURIComponent(element[1]);
+
+    queryObject[paramName] = paramValue;
+  }
+};
