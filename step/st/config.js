a402b1eff9cb67ea14b1507f1e0f8eeb8f6bf04a
studio-dg <81454965+studio-dg@users.noreply.github.com>
2021-04-22 18:25:59 +0900

    Update config.js

--- step/st/config.js
+++ step/st/config.js
@@ -1,11 +1,1 @@
-// コンソールの内容をそのままコピペ
-var config = {
-  apiKey: "AIzaSyCAElLSoR2gD58y8A2QZdj12I-NCBnQa_Y",
-  authDomain: "step-at.firebaseapp.com",
-  databaseURL: "https://step-at.firebaseio.com",
-  projectId: "step-at",
-  storageBucket: "step-at.appspot.com",
-  messagingSenderId: "G-FLKR07TPZY"
-};
-firebase.initializeApp(config);
-
+Q
